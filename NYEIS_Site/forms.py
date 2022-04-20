8192a6340f74c545ed9fd44c9be91ad7d20d3965
deepak <deepak_dit2009@outlook.com>
2022-04-20 10:48:36 -0400

    added new changes

---
+++ NYEIS_Site/forms.py
@@ -0,0 +1,54 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, DateField, EmailField, SelectField ,SubmitField
+
+from wtforms.validators import DataRequired, Length, Email, ValidationError
+
+STATES_TUPLE = [("AL", "Alabama"), ("AK", "Alaska"), ("AZ", "Arizona"), ("AR", "Arkansas"), ("CA", "California"),
+                ("CO", "Colorado"),
+                ("CT", "Connecticut"), ("DC", "Washington DC"), ("DE", "Delaware"), ("FL", "Florida"),
+                ("GA", "Georgia"),
+                ("HI", "Hawaii"), ("ID", "Idaho"), ("IL", "Illinois"), ("IN", "Indiana"), ("IA", "Iowa"),
+                ("KS", "Kansas"), ("KY", "Kentucky"),
+                ("LA", "Louisiana"), ("ME", "Maine"), ("MD", "Maryland"), ("MA", "Massachusetts"), ("MI", "Michigan"),
+                ("MN", "Minnesota"),
+                ("MS", "Mississippi"), ("MO", "Missouri"), ("MT", "Montana"), ("NE", "Nebraska"), ("NV", "Nevada"),
+                ("NH", "New Hampshire"),
+                ("NJ", "New Jersey"), ("NM", "New Mexico"), ("NY", "New York"), ("NC", "North Carolina"),
+                ("ND", "North Dakota"), ("OH", "Ohio"),
+                ("OK", "Oklahoma"), ("OR", "Oregon"), ("PA", "Pennsylvania"), ("RI", "Rhode Island"),
+                ("SC", "South Carolina"), ("SD", "South Dakota"),
+                ("TN", "Tennessee"), ("TX", "Texas"), ("UT", "Utah"), ("VT", "Vermont"), ("VA", "Virginia"),
+                ("WA", "Washington"), ("WV", "West Virginia"),
+                ("WI", "Wisconsin"), ("WY", "Wyoming")]
+
+
+def type_check(form, field):
+    val: str = field.data
+    if not val.isdigit():
+        raise ValidationError('Can contain only numbers')
+
+
+class IntakeForm(FlaskForm):
+    child_firstname = StringField("Child First Name", validators=[DataRequired()])
+    child_middlename = StringField("Child Middle Name", validators=[DataRequired()])
+    child_lastname = StringField("Child Last Name", validators=[DataRequired()])
+    child_dob = DateField("Date Of Birth", validators=[DataRequired()])
+    father_name = StringField("Father Name")
+    father_dob = DateField("Father's Date Of Birth")
+    mother_name = StringField("Mother Name")
+    mother_dob = DateField("Mother's Date Of Birth")
+    save_form = SubmitField('Save')
+
+
+class ContactForm(FlaskForm):
+    ph_areacode = StringField('Area Code', validators=[type_check, Length(min=1, max=3)])
+    ph_phonenumber = StringField('PhoneNumber', validators=[type_check, Length(min=7, max=7)])
+    email_addr = EmailField('Email Address', validators=[Email()])
+
+
+class AddressForm(FlaskForm):
+    address_line1 = StringField('Area Code', validators=[DataRequired(), Length(min=1, max=3)])
+    address_line2 = StringField('Area Code', validators=[Length(min=1, max=3)])
+    address_city = StringField('Area Code', validators=[DataRequired(), Length(min=1, max=3)])
+    address_state = SelectField('State', choices=STATES_TUPLE, validators=[DataRequired()])
+    address_zip = StringField('Zip', validators=[type_check, Length(max=5)])
--- NYEIS_Site/main.py
+++ NYEIS_Site/main.py
@@ -1,15 +1,65 @@
-from flask import Flask, render_template
+import datetime
+
+from flask import Flask, render_template, url_for
+from forms import IntakeForm, AddressForm, ContactForm
+import uuid
 
 app = Flask(__name__)
+app.secret_key = '543af292a648479e77c6b86b13b95179209f77c13bee75ff916d4f888418b09b'
+
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://demo_user:Password123@@localhost:3306/sample_db'
+db = SQLAlchemy(app)
+
+
+class Person(db.Model):
+    id = db.Column(db.String(30), primary_key=True, autoincrement=False, default=uuid.uuid4)
+    child_ref = db.Column(db.String(20), nullable=False)
+    child_firstname = db.Column(db.String(50), nullable=False)
+    child_middlename = db.Column(db.String(50), nullable=False)
+    child_lastname = db.Column(db.String(50), nullable=False)
+    child_dob = db.Column(db.Date(), nullable=False)
+    father_name = db.Column(db.String(100))
+    father_dob = db.Column(db.String(100))
+    mother_name = db.Column(db.String(100))
+    mother_dob = db.Column(db.String(100))
+    created_on = db.Column(db.DateTime())
+    updated_on = db.Column(db.DateTime(), default=datetime.datetime)
+    addresses = db.relationship('Address', backref='person', lazy=True)
+    contact = db.relationship('Contact', backref='person', lazy=True)
+
+
+class Address(db.Model):
+    id = db.Column(db.String(30), primary_key=True, autoincrement=False, default=uuid.uuid4)
+    address_line1 = db.Column(db.String(120), nullable=False)
+    address_line2 = db.Column(db.String(120), nullable=False)
+    address_city = db.Column(db.String(120), nullable=False)
+    address_state = db.Column(db.String(120), nullable=False)
+    address_zip = db.Column(db.String(120), nullable=False)
+    created_on = db.Column(db.DateTime())
+    updated_on = db.Column(db.DateTime(), default=datetime.datetime)
+    person_id = db.Column(db.String(30), db.ForeignKey('person.id'), nullable=False)
+
+
+class Contact(db.Model):
+    id = db.Column(db.String(30), primary_key=True, autoincrement=False, default=uuid.uuid4)
+    ph_areacode = db.Column(db.Integer)
+    ph_phonenumber = db.Column(db.Integer)
+    email_addr = db.Column(db.String(50))
+    person_id = db.Column(db.String(30), db.ForeignKey('person.id'), nullable=False)
+
 
 children = [
     {
+        'ref_no': 12345,
         'first': 'Alpha',
         'last': 'Pandey',
         'address': '10 Main Road, Texas, US',
         'dob': '15 April 2022'
     },
     {
+        'ref_no': 64789,
         'first': 'Beta',
         'last': 'Pandey',
         'address': '10 Second Road, Texas, US',
@@ -22,12 +72,24 @@ children = [
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', children=children , title='Children List')
+    return render_template('home.html', children=children, title='Children List')
 
 
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    intakeForm = IntakeForm()
+    contactForm = ContactForm()
+    addressForm = AddressForm()
+    if intakeForm.validate_on_submit() and contactForm.validate_on_submit() and addressForm.validate_on_submit():
+        pass
+
+    return render_template('register.html', intakef=intakeForm, contf=contactForm, addrf=addressForm,
+                           title='Registration Form')
 
 
 if __name__ == '__main__':
